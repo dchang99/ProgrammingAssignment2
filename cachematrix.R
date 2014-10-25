b1f73798c536842d1ae5add4ed4c07306b2f6716
dchang99 <bruceleedc@gmail.com>
2014-10-25 16:29:45 -0700

    R Programming - Programming Assignment 2, Submission1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Class: CacheMatrix
+## ------------------
+## Matrix inversion is usually a costly computation. This class
+## allows caching of the inverse of a matrix rather than computing
+## it repeatedly.
 
-## Write a short comment describing this function
+## Function: makeCacheMatrix
+## -------------------------
+## Creates an object that can store a matrix 'x' and cache its inverse.
+##      set: Set stored matrix
+##      get: Retrieve stored matrix
+##      setInverse: Set cached inverse of stored matrix
+##      getInverse: Get cached inverse of stored matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+         x_inverse <- NULL
+         
+         set <- function(y) {
+                 x <<- y
+                 x_inverse <<- NULL
+         }
 
+         get <- function() x
+         
+         setInverse <- function(inverse) x_inverse <<- inverse
+         
+         getInverse <- function() x_inverse
+         
+         list(set = set, get = get, 
+              setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Function: cacheSolve
+## --------------------
+## Return a matrix that is the inverse of makeCacheMatrix 'x'.
+## The inverse is cached after the first time cacheSolve is
+## called; subsequent calls return the cached value.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        if(is.null(x$get())) return(NULL)
+        
+        x_inverse <- x$getInverse()
+        
+        if(is.null(x_inverse)) {
+                x_inverse <- solve(x$get())
+                x$setInverse(x_inverse)
+        }
+        
+        x_inverse
 }
